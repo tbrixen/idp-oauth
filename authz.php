8975be5dba0ba2243ab77cd51b50dcad88fc05cb
Tobias Brixen <tbrixen@gmail.com>
2014-11-09 14:52:53 +0100

    A user can now login

--- authz.php
+++ authz.php
@@ -1,3 +1,6 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+?>
 <html>
 <head>
 <title>Auth server</title>
@@ -7,17 +10,30 @@
 <h1>Hi and welcome to the authz server.</h1>
 <?php
 
-$action = $_GET['action'];
 
-if (isset($action)){
+if (isset($_GET['action'])){
+    $action = $_GET['action'];
+
     switch($action){
-        case "ab":
-            handleAb();
+        case "redirect":
+            handleRedirect();
             break;
         default:
             handleDefault();
     }
+} else if (isset($_POST['commit'])){
+    // Check of user credentials are corret
+    if($_POST['username'] == "ole" && $_POST['password'] == "bole") {
+        echo "correct";
+    } else {
+        echo "Not correct credentials.";
+    }
+
+
+
 } else {
+    // An actions hasn't been set, and there hasn't been amy attempt to log on
+
 
     echo '<p>Here you can login to authorize yoursef.</p>';
 
@@ -27,3 +43,62 @@ if (isset($action)){
 ?>
 </body>
 </html>
+<?php
+
+function handleRedirect()
+{
+    $clientId = $_GET['clientid'];
+    $responseType = $_GET['responsetype'];
+    $scope = $_GET['scope'];
+
+
+    $clientName = "undefined";
+    switch($clientId){
+        case "42":
+            $clientName = "BookFace";
+    }
+
+    $scopeText = "undefined";
+    switch($scope){
+        case "1":
+            $scopeText = "<li>First name</li>";
+            break;
+        case "2":
+            $scopeText = "<li>First name</li><li>Last name</li>";
+            break;
+        case "3":
+            $scopeText = "<li>First name</li><li>Last 
+                name</li><li>Birthday</li>";
+            break;
+    }
+
+    // The user should now get to authorize 
+    echo "Would you like to give " . $clientName . " access to: 
+        <ul> " . $scopeText . "</li><br />";
+
+    echo "Then please login using the following form:";
+    echo '
+        <form method="post" action="authz.php">
+        <input name="username" type="text" value="" placeholder="Username">
+        <input name="password" type="password" value="" placeholder="Password">
+        <input name="clientid" type="hidden" value="' . $clientId . '">
+        <input name="responsetype" type="hidden" value="' . $responseType . '">
+        <input name="scope" type="hidden" value="' . $scope . '">
+        <input name="commit" type="submit" value="Login">
+        </form>
+        ';
+    
+
+    
+}
+
+
+function handleDefault()
+{
+    echo "<p>Action not defined</p><br />Get parameters: <br />";
+    foreach($_GET as $key => $value){
+        echo $key . " : " . $value . "<br />\r\n";
+    }
+}
+
+?>
--- client.php
+++ client.php
@@ -5,13 +5,13 @@
 
 <body>
 
-<h1>Hi and welcome to the client site</h1>
+<h1>Hi and welcome to BookFace (the client site).</h1>
 
 <?php
 
-$action = $_GET['action'];
 
-if (isset($action)){
+if (isset($_GET['action'])){
+    $action = $_GET['action'];
     switch($action){
         case "ab":
             handleAb();
@@ -21,9 +21,18 @@ if (isset($action)){
     }
 } else {
 
-    echo '<p>So you want to use our sevices? Then i need data from the authz 
-        controlled resource server. Please give us access by <a 
-        href="authz.php">logging in</a></p>.';
+    echo '<p>So you want to use our sevices? Then we need data from the authz 
+        controlled resource server. The more data you\'ll give us, the more 
+        points you\'ll get. Choose between:
+
+        <ul>
+        <li><a href="authz.php?action=redirect&clientid=42&responsetype=acg&scope=1">1 point</a>: First name</li>
+        <li><a href="authz.php?action=redirect&clientid=42&responsetype=acg&scope=2">2 points</a>: First and last name</li>
+        <li><a href="authz.php?action=redirect&clientid=42&responsetype=acg&scope=3">3 points</a>: First and last name + birthday</li>
+        </ul>
+
+        
+        ';
 
 }
 
